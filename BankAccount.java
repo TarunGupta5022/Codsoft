66ac16fe8d1ea6ec12f04c9f80c620fba9df83ca
TarunGupta5022 <144485672+TarunGupta5022@users.noreply.github.com>
2023-09-09 00:24:18 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+        }
+    }
+}
+
+class ATM {
+    private BankAccount account;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+    }
+
+    public void displayMenu() {
+        System.out.println("Welcome to the ATM!");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            displayMenu();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 4);
+    }
+
+    public void checkBalance() {
+        double balance = account.getBalance();
+        System.out.println("Your account balance: $" + balance);
+    }
+
+    public void deposit() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the deposit amount: $");
+        double amount = scanner.nextDouble();
+        account.deposit(amount);
+    }
+
+    public void withdraw() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the withdrawal amount: $");
+        double amount = scanner.nextDouble();
+        account.withdraw(amount);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000.0); // Initial balance of $1000
+        ATM atm = new ATM(userAccount);
+        atm.run();
+    }
+}
+
---
+++ NumberGuessingGame.java
@@ -0,0 +1,58 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int lowerBound = 1;
+        int upperBound = 100;
+        int maxAttempts = 10;
+        int rounds = 0;
+        int score = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("You have " + maxAttempts + " attempts to guess the number between "
+                + lowerBound + " and " + upperBound + ".");
+
+        boolean playAgain = true;
+        while (playAgain) {
+            int secretNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == secretNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    guessedCorrectly = true;
+                    score++;
+                    break;
+                } else if (userGuess < secretNumber) {
+                    System.out.println("Too low. Try again.");
+                } else {
+                    System.out.println("Too high. Try again.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("You've used all your attempts. The correct number was: " + secretNumber);
+            }
+
+            rounds++;
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainChoice = scanner.next().toLowerCase();
+            if (!playAgainChoice.equals("yes")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Thanks for playing! Your score is: " + score + " out of " + rounds + " rounds.");
+        scanner.close();
+    }
+}
+
